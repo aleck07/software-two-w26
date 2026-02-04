4fd8435720d58ab08cbe738a23b4d32239fefb7f
Alec Krsek <aleckrsek@gmail.com>
2026-02-04 15:06:14 -0800

    Add breakout

--- week5/day2/breakout.rb
+++ week5/day2/breakout.rb
@@ -13,6 +13,30 @@ class DatabaseConnection
   end
 end
 
+class MockDatabaseConnection
+  def connect
+    # do nothing
+  end
+
+  def add_message(m)
+    # do nothing
+  end
+end
+
+class MockMessage
+    attr_accessor :text, :sent_message
+
+    def initialize(text)
+        @db = MockDatabaseConnection.new
+        @text = text
+        @sent_message = nil
+    end
+
+    def send()
+        @sent_message = @text
+    end
+end
+
 class Message
   attr_accessor :text, :sent_message
 
@@ -23,15 +47,27 @@ class Message
   end
 
   def send()
-    @db.connect
-    @db.add_message(@text)
-    @sent_message = @text
+    message = MessageSender.new(self, @db)
   end
+
 end
 
-class TestMessage < MiniTest::Test
+class MessageSender 
+    def initialize(message, db_connection)
+        @message = message
+        @db = db_connection
+    end
+
+    def send()
+        @db.connect
+        @db.add_message(@message.text)
+        @sent_message = @message.text
+    end
+end
+
+class TestMessage < Minitest::Test
   def test_message
-    m = Message.new("Hi there")
+    m = MockMessage.new("Hi there")
     m.send()
     assert_equal "Hi there", m.sent_message
   end
